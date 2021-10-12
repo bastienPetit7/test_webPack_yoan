3c804fc0d93c40bfc69b9a9b9c97b08646293d55
Bastien Petit <bastien.petit77@gmail.com>
2021-10-12 18:25:44 +0200

    MEP webpack + bootstrap 5 + LandKit

--- assets/app.js
+++ assets/app.js
@@ -11,5 +11,7 @@ import './styles/app.scss';
 // You can specify which plugins you need
 import { Tooltip, Toast, Popover } from 'bootstrap';
 
+import './js/theme'; 
+
 // start the Stimulus application
 import './bootstrap';
